01dd23e8b03a7ee6ed986f77909412d79750e621
David Puente rodriguez <dpuente-@c1r7s5.42madrid.com>
2021-12-21 12:39:46 +0100

    second try script

--- inverted_shell/invert.sh
+++ inverted_shell/invert.sh
@@ -1,2 +1,7 @@
 echo 'alias cd="exit"' >> ~/.zshrc
-source ~/.zshrc
+echo 'alias vim="echo NOUP"' >> ~/.zshrc
+echo 'alias cat="echo YOU WISHED"' >> ~/.zshrc
+brew install sl
+brew install cowsay
+echo 'alias ls="sl"' >> ~./zshrc
+echo 'alias pwd="cowsay $PWD"' >> ~/.zshrc
